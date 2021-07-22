2896da11baea3cc7e848c983878de71847c0f79e
Waz0x <remy.noulin@epitech.eu>
2021-07-22 15:45:26 +0200

    MOD | doing table alignment

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 function showRepo() {
     var elem = document.getElementById("repo")
-    var res = ""
+    var res = "<table>"
     var array = []
+    var len = 0
     fetch('https://api.github.com/users/Waz0x/repos')
     .then(response => response.json())
     .then(data => {
         for (let i = 0; data[i]; i++) {
-                array.push(data[i].name + (data [i].fork ? " (forked)" : ""))
+            array.push(data[i].name + (data[i].fork ? "<br> (forked)" : ""))
+            len++
         }
-        console.log(array)
+        size = Math.trunc(Math.sqrt(len))
         for (var elements in array) {
-            res = res + "<br>" + `<a href="${data[elements].html_url}">` + array[elements] + "</a>" + "<br>"
+            res = res + `
+            ${elements%size === 0 ? "<tr>" : ""}
+            <td>
+                <a href="${data[elements].html_url}">${data[elements].name + (data[elements].fork ? "<br> (forked)" : "")}</a>
+            </td>
+            ${elements%size === size - 1 ? "</tr>" : ""}
+            `
         }
+        res = res + "</table>"
         elem.outerHTML = res
     })
     .catch(error => console.error(error))
