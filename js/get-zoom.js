3e01215cafdd5d207331dc5adffd92764418ade9
Waz0x <remy.noulin@epitech.eu>
2021-07-22 11:48:02 +0200

    MOD | renamed page

--- js/get-zoom.js
+++ js/get-zoom.js
@@ -1,6 +1,6 @@
 $(window).resize(function() { 
     var browserZoomLevel = Math.round(window.devicePixelRatio * 100);
     if (browserZoomLevel <= 99) {
-        location.replace('./cheh.html')
+        location.replace('./dont_zoom_out.html')
     }
 });
