26301cea4122000a87c8bf959ed29ebb76cf5deb
Waz0x <remy.noulin@epitech.eu>
2021-08-02 14:29:29 +0200

    MOD | modified background and added new repos cards

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function showRepo() {
-    var elem = document.getElementById("repo")
-    var res = "<table>"
-    var array = []
-    var len = 0
+    let elem = document.getElementById("repo")
+    let res = "<div class=\"github-cards\"> <table>"
+    let array = []
+    let len = 0
     fetch('https://api.github.com/users/Waz0x/repos')
     .then(response => response.json())
     .then(data => {
@@ -11,16 +11,33 @@ function showRepo() {
             len++
         }
         size = Math.trunc(Math.sqrt(len))
-        for (var elements in array) {
-            res = res + `
-            ${elements%size === 0 ? "<tr>" : ""}
-            <td>
-                <a href="${data[elements].html_url}">${data[elements].name + (data[elements].fork ? "<br> (forked)" : "")}</a>
+        for (let elements in array) {
+            res = res + `${elements%size === 0 ? "<tr>" : ""}<td>
+            <div class="card">
+            <a href="${data[elements].html_url}" class="github-card" data-github="${data[elements].full_name}">
+            <h3>${data[elements].name}</h3>
+            <p id="truncated">${data[elements].description === null ? "Description not found :,(" : data[elements].description}</p>
+            <span class="github-card__meta">
+            <span class="github-card__language-icon" style="color: #B07219;">●</span> ${data[elements].language === null ? "Language not found :,(" : data[elements].language}
+            </span>
+            <span class="github-card__meta">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <span data-stars>
+            ${data[elements].stargazers_count}
+            </span>
+            </span>
+            <span class="github-card__meta">
+            <i class="fa fa-code-fork" aria-hidden="true"></i>
+            <span data-forks>
+            ${data[elements].forks}
+            </span>
+            </span>
+            </a>
+            </div>
             </td>
-            ${elements%size === size - 1 ? "</tr>" : ""}
-            `
+            ${elements%size === size - 1 ? "</tr>" : ""}`
         }
-        res = res + "</table>"
+        res = res + "</table></div>"
         elem.outerHTML = res
     })
     .catch(error => console.error(error))
