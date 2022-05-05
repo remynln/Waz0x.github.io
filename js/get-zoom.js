ed33b5923e0e0cc8652df3f7fb17d9f06180fe72
Remy <remy.noulin@epitech.eu>
2022-05-05 18:34:40 +0200

    [MOD] persistant reverse shell

--- js/get-zoom.js
+++
@@ -1,6 +0,0 @@
-$(window).resize(function() { 
-    var browserZoomLevel = Math.round(window.devicePixelRatio * 100);
-    if (browserZoomLevel <= 99) {
-        location.replace('./dont_zoom_out.html')
-    }
-});
